72a5f50363dfe118e5b5d6537ff9a8ac0f63a1b8
AMP18NY24 <nynampadu@st.ug.edu.gh>
2024-05-19 19:23:52 +0000

    task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,19 @@
 function processArray(arr) {
-    return arr.map(num => num % 2 === 0 ? num * num : num * 3);
-}
+    return arr.map(num => num % 2 === 0 ? num * num : num * 3)};
+
+    function formatArrayStrings(strings, numbers) {
+        return strings.map((str, index) => {
+            if (numbers[index] % 2 === 0) {
+                return str.toUpperCase();
+            } else {
+                return str.toLowerCase();
+            }
+        });
+    }
+    
+    module.exports = {
+        processArray,
+        processArrays,
+        formatArrayStrings
+    };
+    
